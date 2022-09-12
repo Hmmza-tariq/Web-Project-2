6bc42578a98b4718240573bcfb2d7d1c39e31942
Hmmza-tariq <itsjohndalton@gmail.com>
2022-09-13 00:40:41 +0500

    files

--- menu.js
+++ menu.js
@@ -7,13 +7,25 @@ function loadpage() {
 };
 //****************************************************************************************************************** */
 //****************************************************************************************************************** */
+document.querySelector('.home').addEventListener('mouseenter', menu_in);
+document.querySelector('.hm').addEventListener('mouseleave', menu_out);
+
+function menu_in(){
+    document.querySelector('.homesect').classList.remove('visible');
+}
+
+function menu_out(){
+    document.querySelector('.homesect').classList.add('visible');
+}
+//****************************************************************************************************************** */
+//****************************************************************************************************************** */
 
 let sld = document.querySelector('.slider1sect');
 let in_sld = document.querySelector('.ssInner');
- sld.onmousedown()
+
 let pressed = false;
 let startX;
-let d;
+let w;
 
 sld.addEventListener('mousedown', (e) => {
     pressed = true;
@@ -31,9 +43,8 @@ sld.addEventListener('mouseup', () => {
 sld.addEventListener('mousemove', (e) => {
     if (!pressed) return;
     e.preventDefault();
-    d = e.offsetX;
-    in_sld.style.left = (d - startX);
-    console.log(d - startX);
+    w = e.offsetX;
+    in_sld.style.left = `${w - startX}px`;
     checkBoundary();
 });
 function checkBoundary() {
@@ -41,12 +52,14 @@ function checkBoundary() {
     let inner = in_sld.getBoundingClientRect();
 
     if (parseInt(in_sld.style.left) > 0) {
-        in_sld.style.left = `0`;
-    } else if (inner.right > outer.right) {
-        in_sld.style.left = `-(inner.width - outer.width)`;
+        in_sld.style.left = `0px`;
+    }
+    if (inner.right < outer.right) {
+        in_sld.style.left = `-${inner.width - outer.width}px`;
     }
 }
-//****************************************************************************************************************** */
+
+// ****************************************************************************************************************** */
 //****************************************************************************************************************** */
 
 let rv = document.querySelector('.rws');
@@ -91,7 +104,7 @@ let in_sld_2 = document.querySelector('.ssInner_2');
 
 let pressed_2 = false;
 let startX_2;
-let d_2;
+let w_2;
 
 sld_2.addEventListener('mousedown', (e) => {
     pressed_2 = true;
@@ -109,18 +122,18 @@ sld_2.addEventListener('mouseup', () => {
 sld_2.addEventListener('mousemove', (e) => {
     if (!pressed_2) return;
     e.preventDefault();
-    d_2 = e.offsetX;
-    in_sld_2.style.left = (d_2 - startX_2);
+    w_2 = e.offsetX;
+    in_sld_2.style.left = (w_2 - startX_2);
     checkBoundary_2();
 });
 function checkBoundary_2() {
     let outer_2 = sld_2.getBoundingClientRect();
     let inner_2 = in_sld_2.getBoundingClientRect();
-    
+
     if (parseInt(in_sld_2.style.left) > 0) {
-        in_sld_2.style.left = `0`;
-    } else if (inner_2.right > outer_2.right) {
-        in_sld_2.style.left = `-(inner_2.width - outer_2.width)`;
+        in_sld_2.style.left = `0px`;
+    } else if (inner_2.right < outer_2.right) {
+        in_sld_2.style.left = `-${inner_2.width - outer_2.width}px`;
     }
 }
 //****************************************************************************************************************** */
@@ -128,6 +141,66 @@ function checkBoundary_2() {
 
 let mld = document.querySelector('.ms');
 
-mld.addEventListener('mouseenter',()=>{
-    
+mld.addEventListener('mouseenter', () => {
+
 })
+//****************************************************************************************************************** */
+//****************************************************************************************************************** */
+
+function counter() {
+    let i = 0, j = 0, k = 0, l = 0;
+    const myInterval_1 = setInterval(() => {
+        document.querySelector('.countgp1').innerText = `${i}+`;
+        i += 100;
+        if (i > 6000) {
+            clearInterval(myInterval_1);
+        }
+    }, 10);
+
+    const myInterval_2 = setInterval(() => {
+        document.querySelector('.countgp2').innerText = `${j}+`;
+        j++;
+        if (j > 90) {
+            clearInterval(myInterval_2);
+        }
+    }, 10)
+
+    const myInterval_3 = setInterval(() => {
+        document.querySelector('.countgp3').innerText = `${k}+`;
+        k++;
+        if (k > 10) {
+            clearInterval(myInterval_3);
+        }
+    }, 50)
+
+    const myInterval_4 = setInterval(() => {
+        document.querySelector('.countgp4').innerText = `${l}%`;
+        l++;
+        if (l > 100) {
+            clearInterval(myInterval_4);
+        }
+    }, 10)
+}
+
+let cnt = false;
+window.addEventListener('scroll', reveal);
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal');
+    
+    for (let i = 0; i < reveals.length; i++) {
+
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 70;
+        if (revealTop < windowHeight - revealPoint) {
+                    reveals[i].classList.add('active_1')
+                if (cnt === false) {
+                    counter();
+                    cnt = true;
+            }
+
+        }
+    }
+}
+//****************************************************************************************************************** */
+//****************************************************************************************************************** */
