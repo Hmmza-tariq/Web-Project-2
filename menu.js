60b38228c200b98df1cd95c7f684cdd62b0faa12
Hmmza-tariq <111647708+Hmmza-tariq@users.noreply.github.com>
2022-09-13 21:13:27 +0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -12,13 +12,13 @@ function openNav() {
     document.querySelector(".head").style.width = "300px";
     document.querySelector(".navmenu").style.marginLeft = "300px";
     document.querySelector('.home').innerHTML = 'Home<hr><span class="inner"><ul>Main Home </ul><ul>Home SAAS </ul><ul>Home Business </ul><ul>Home Political </ul><ul>Home Education </ul><ul>Home Event </ul><ul>Home Church </ul></span><hr>';
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
     document.querySelector(".head").style.width = "0";
-    document.querySelector(".navmenu").style.marginLeft= "0";
+    document.querySelector(".navmenu").style.marginLeft = "0";
     document.querySelector('.home').innerHTML = 'Home';
-  }
+}
 
 //****************************************************************************************************************** */
 //****************************************************************************************************************** */
@@ -26,11 +26,11 @@ function openNav() {
 document.querySelector('.home').addEventListener('mouseenter', menu_in);
 document.querySelector('.hm').addEventListener('mouseleave', menu_out);
 
-function menu_in(){
+function menu_in() {
     document.querySelector('.homesect').classList.remove('visible');
 }
 
-function menu_out(){
+function menu_out() {
     document.querySelector('.homesect').classList.add('visible');
 }
 
@@ -58,6 +58,23 @@ sld.addEventListener('mouseup', () => {
     pressed = false;
 });
 sld.addEventListener('mousemove', (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+    w = e.offsetX;
+    in_sld.style.left = `${w - startX}px`;
+    checkBoundary();
+});
+
+sld.addEventListener('touchstart', (e) => {
+    pressed = true;
+    startX = e.offsetX - in_sld.offsetLeft;
+    sld.style.cursor = 'grabbing';
+});
+sld.addEventListener('touchend', () => {
+    sld.style.cursor = 'grab';
+    pressed = false;
+});
+sld.addEventListener('touchmove', (e) => {
     if (!pressed) return;
     e.preventDefault();
     w = e.offsetX;
@@ -143,6 +160,24 @@ sld_2.addEventListener('mousemove', (e) => {
     in_sld_2.style.left = (w_2 - startX_2);
     checkBoundary_2();
 });
+
+sld_2.addEventListener('touchstart', (e) => {
+    pressed_2 = true;
+    startX_2 = e.offsetX - in_sld_2.offsetLeft;
+    sld_2.style.cursor = 'grabbing';
+});
+
+sld_2.addEventListener('touchend', () => {
+    sld_2.style.cursor = 'grab';
+    pressed_2 = false;
+});
+sld_2.addEventListener('touchmove', (e) => {
+    if (!pressed_2) return;
+    e.preventDefault();
+    w_2 = e.offsetX;
+    in_sld_2.style.left = (w_2 - startX_2);
+    checkBoundary_2();
+});
 function checkBoundary_2() {
     let outer_2 = sld_2.getBoundingClientRect();
     let inner_2 = in_sld_2.getBoundingClientRect();
@@ -203,17 +238,17 @@ let cnt = false;
 window.addEventListener('scroll', reveal);
 function reveal() {
     let reveals = document.querySelectorAll('.reveal');
-    
+
     for (let i = 0; i < reveals.length; i++) {
 
         let windowHeight = window.innerHeight;
         let revealTop = reveals[i].getBoundingClientRect().top;
         let revealPoint = 70;
         if (revealTop < windowHeight - revealPoint) {
-                    reveals[i].classList.add('active_1')
-                if (cnt === false) {
-                    counter();
-                    cnt = true;
+            reveals[i].classList.add('active_1')
+            if (cnt === false) {
+                counter();
+                cnt = true;
             }
 
         }
